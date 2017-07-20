1b7ecf4efd329c61bdf135c946500d5abb77bb72
wbdana <william.b.dana@gmail.com>
2017-07-20 15:31:27 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    window.alert("You CLICKED!")
+  })
+};
+
+function frameIt() {
+  $('img').addClass("tasty")
+};
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71){
+      alert('G key was pressed down');
+    }
+  })
+};
+
+function submitIt() {
+  $('form').on('submit', function() {
+    window.alert('Your form is going to be submitted now.')
+  })
+};
+
+
 $(document).ready(function(){
 
 // call functions here
+
 
 });
